47a0ff410fcdf239b4ff30411b77c8a9f21df1d7
Koichi Murakami <koichi.murakami@kek.jp>
2022-09-26 02:47:21 +0900

    add comfig cmake

---
+++ config.cmake
@@ -0,0 +1,28 @@
+#==============================================================================
+# CMake configuration for G4Bench2
+#
+# NOTE:
+# CACHE variables can be changed in CMake CLI with -D option.
+#==============================================================================
+# installation prefix
+if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+  set(CMAKE_INSTALL_PREFIX $ENV{HOME}/work/g4bench/work
+      CACHE PATH "Install prefix" FORCE)
+endif()
+
+# Geant4 installation path
+set(GEANT4_INSTALL /opt/geant4/11.0.3
+    CACHE STRING "Geant4 installation path")
+
+# visualization flag
+set(ENABLE_VIS FALSE CACHE BOOL "Enable visualization flag")
+
+# Optimizaton / Debug flags
+set(OPTIMIZE TRUE CACHE BOOL "Optimizaton flag (O3)")
+set(DEBUG FALSE CACHE BOOL "Debug mode")
+
+# Development flag (set false for release)
+set(DEVMODE FALSE CACHE BOOL "Development mode")
+
+# spacial flags for developer
+option(GEANT4_STATIC "Use static library" OFF)
